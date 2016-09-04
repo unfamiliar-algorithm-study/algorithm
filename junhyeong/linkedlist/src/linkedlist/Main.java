0f19c8153da93e5eda59b9f826cd6f5dc6a147b9
leejunhyeong <lhyoup0360@naver.com>
2016-09-04 19:03:48 +0900

    linkedlist

---
+++ junhyeong/linkedlist/src/linkedlist/Main.java
@@ -0,0 +1,28 @@
+package linkedlist;
+
+public class Main {
+	public static void main(String[] args){
+		LinkedList numbers = new LinkedList();
+		numbers.addFirst(50);
+		numbers.addFirst(10);
+		numbers.add(2, 100);
+		numbers.addLast(20);
+		numbers.addLast(30);
+
+		System.out.println(numbers.node(20));
+		System.out.println("»çÀÌÁî = " + numbers.size());
+		System.out.println("index '3' = " + numbers.get(3));
+		System.out.println("index of '30' = " + numbers.indexOf(30));
+		
+		LinkedList.ListIterator li = numbers.listIterator();
+		System.out.println("use iterator first = " + li.next());
+		System.out.println("use iterator second = " + li.next());
+		
+		while(li.hasNext()){
+		    if((int)li.next() == 20)
+		    	li.add(40);
+		}		
+		System.out.println(numbers);
+		
+	}
+}

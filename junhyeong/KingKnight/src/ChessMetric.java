24f241a8264549ab4a1887d6232ada102b2ca16d
leejunhyeong <lhyoup0360@naver.com>
2016-11-18 22:55:55 +0900

    킹나이트 체스 코드 추가

---
+++ junhyeong/KingKnight/src/ChessMetric.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class ChessMetric {
+	static int[][] dp;
+	
+	public static void main(String[] args){
+		int size;
+		int[] start = new int[2];
+		int[] end = new int[2];
+		int numMoves;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Size = ");
+		size = sc.nextInt();
+		
+		dp = new int[size+1][size+1];
+		
+		System.out.println("start = ");
+		for(int i = 0; i <2 ; i++){
+		start[i] = sc.nextInt();
+		}
+		
+		System.out.println("end = ");
+		for(int i = 0; i <2 ; i++){
+		end[i] = sc.nextInt();
+		}
+		
+		System.out.println("numMoves = ");
+		numMoves = sc.nextInt();
+		
+		howMany(size, start, end, numMoves);
+		
+		System.out.println(dp[end[0]][end[1]]);
+	}
+	
+	public static long howMany(int size, int[] start, int[] end, int numMoves){
+
+		int[] temp = new int[2];
+		int[] move_x = {0, 0, 1, 1, 1, -1, -1, -1, 2, 2, -2, -2, 1, -1, 1, -1};
+		int[] move_y = {1, -1, 1, 0, -1, 1, 0, -1, 1, -1, 1, -1, 2, 2, -2, -2};
+
+		for(int i = 0; i < 16; i++){
+			temp[0] = start[0] + move_x[i];
+			temp[1] = start[1] + move_y[i];
+			if(temp[0] >= 0 && temp[1] >= 0 && temp[0] <= size && temp[1] <= size){
+				dp[temp[0]][temp[1]] += 1;
+				
+				if(numMoves > 1){
+					howMany(size, temp, end, numMoves-1);
+				}
+			}
+		}
+
+		return 0;
+	}
+		
+	public static void move(int[] start, int[] end, int numMoves){
+
+		int[] temp = new int[2];
+		int[] move_x = {0, 0, 1, 1, 1, -1, -1, -1, 2, 2, -2, -2, 1, -1, 1, -1};
+		int[] move_y = {1, -1, 1, 0, -1, 1, 0, -1, 1, -1, 1, -1, 2, 2, -2, -2};
+
+		for(int i = 0; i < 16; i++){
+			temp[0] = start[0] + move_x[i];
+			temp[1] = start[1] + move_y[i];
+			if(temp[0] >= 0 && temp[1] >= 0 && temp[0] <= size && temp[1] <= end[1]){
+				dp[temp[0]][temp[1]] += 1;
+				
+				if(numMoves > 1){
+					move(temp, end, numMoves-1);
+				}
+			}
+		}
+		
+	}
+		
+}
